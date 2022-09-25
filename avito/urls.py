4e1abd79a9c567ec5996f897da005ccabac9500d
Armen-ar <armen.aruts1967@gmail.com>
2022-09-25 19:26:08 +0300

    initial

--- avito/urls.py
+++ avito/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ads.views import root, CategoryView, CategoryDetailView, AdView, AdDetailView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', root),
+    path('cat/', CategoryView.as_view()),
+    path('cat/<int:pk>', CategoryDetailView.as_view()),
+    path('ad/', AdView.as_view()),
+    path('ad/<int:pk>', AdDetailView.as_view()),
 ]
