7047c91f9e11333f7000fe03c46d5ab9346ae7b9
Armen-ar <armen.aruts1967@gmail.com>
2022-10-16 21:50:42 +0300

    adding to admin collections

--- ads/admin.py
+++ ads/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from ads.models import Ad, Category
+from ads.models import Ad, Category, Selection
 from users.models import User, Location
 
 admin.site.register(Ad)
+admin.site.register(Selection)
 admin.site.register(Category)
 
 admin.site.register(User)
